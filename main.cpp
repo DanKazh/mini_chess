540de88742dc0c244196a0518270a63095c4a6ba
DanKazh <117713084+DanKazh@users.noreply.github.com>
2024-03-24 21:23:52 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,26 @@
-#include <iostream>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include "window.hpp"
 
-using namespace std;
+const char* defaultTheme[12] = { "D:/chesss/chesss-project/chesss-project/pieces/wp.png",
+                                "D:/chesss/chesss-project/chesss-project/pieces/wr.png",
+                                "D:/chesss/chesss-project/chesss-project/pieces/wn.png",
+                                "D:/chesss/chesss-project/chesss-project/pieces/wb.png",
+                                "D:/chesss/chesss-project/chesss-project/pieces/wq.png",
+                                "D:/chesss/chesss-project/chesss-project/pieces/wk.png",
+                                "D:/chesss/chesss-project/chesss-project/pieces/bp.png",
+                                "D:/chesss/chesss-project/chesss-project/pieces/br.png",
+                                "D:/chesss/chesss-project/chesss-project/pieces/bn.png",
+                                "D:/chesss/chesss-project/chesss-project/pieces/bb.png",
+                                "D:/chesss/chesss-project/chesss-project/pieces/bq.png",
+                                "D:/chesss/chesss-project/chesss-project/pieces/bk.png" };
 
 int main()
 {
-  cout << "Hello World!" << endl;
+    chessWindow window(800, 800, "chess", defaultTheme);
+    while (window.Update())
+    {
+    }
+    return 0;
 }
