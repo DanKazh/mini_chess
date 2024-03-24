34c36cfdb7ef8f3bd614abba584e2555d85ef930
DanKazh <117713084+DanKazh@users.noreply.github.com>
2024-03-24 16:19:44 +0300

    Update window.hpp

--- window.hpp
+++ window.hpp
@@ -13,9 +13,9 @@ struct chessSquare {
 class chessWindow {
 private:
 	sf::RenderWindow window;
-	sf::RectangleShape Board[8][8];
-	sf::Color Colours[2];
-        sf::IntRect Holder;
+	sf::RectangleShape board[8][8];
+	sf::Color colours[2];
+        sf::IntRect holder;
 	sf::Texture pieceTexture[12];
 	chessSquare squares[64];
 	chessBoard newBoard;
