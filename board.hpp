0d6241355c75097481447cd9ff3aaff0366620d7
DanKazh <117713084+DanKazh@users.noreply.github.com>
2024-05-18 17:44:45 +0300

    Update board.hpp

--- board.hpp
+++ board.hpp
@@ -1,13 +1,39 @@
-class chessBoard {
+#include <vector>
+struct move
+{
+    int oX, oY, X, Y;
+    move() {}
+    move(int oldX, int oldY, int newX, int newY);
+};
+struct board
+{
+    int arr[8][8] = { {-1, -1, -1, -1, -1, -1, -1, -1},
+                     {-1, 7, 6, -1, -1, 0, 1, -1},
+                     {-1, 8, 6, -1, -1, 0, 2, -1},
+                     {-1, 9, 6, -1, -1, 0, 3, -1},
+                     {-1, 11, 6, -1, -1, 0, 5, -1},
+                     {-1, 10, 6, -1, -1, 0, 4, -1},
+                     {-1, 7, 6, -1, -1, 0, 1, -1},
+                     {-1, -1, -1, -1, -1, -1, -1, -1} };
+};
+class chessBoard
+{
+private:
+    bool turn = 1;
+    const char* notation(int x, int y);
+    std::vector<board> history;
+
+    void wPawn(std::vector<move>& moves, int x, int y);
+    void bPawn(std::vector<move>& moves, int x, int y);
+    void King(std::vector<move>& moves, int x, int y);
+    void Queen(std::vector<move>& moves, int x, int y);
+    void Rook(std::vector<move>& moves, int x, int y);
+    void Bishop(std::vector<move>& moves, int x, int y);
+    void Knight(std::vector<move>& moves, int x, int y);
+    std::vector<move> getLegalMoves(board b, bool color);
+
 public:
-	int mini_board[8][8] = {
-		{-1, -1, -1, -1, -1, -1, -1, -1},
-		{-1, 7, 6, -1, -1, 0, 1, -1},
-		{-1, 8, 6, -1, -1, 0, 2, -1},
-		{-1, 9, 6, -1, -1, 0, 3, -1},
-		{-1, 10, 6, -1, -1, 0, 4, -1},
-		{-1, 11, 6, -1, -1, 0, 5, -1},
-		{-1, 7, 6, -1, -1, 0, 1, -1},
-		{-1, -1, -1, -1, -1, -1, -1, -1},
-	};
+    board mBoard;
+    bool playMove(move req);
+    bool nextTurn();
 };
